10fa242fa8cdb154f467dc654e80803a9d637260
William Kwasidev <kwesidev@gmail.com>
2017-12-29 07:40:23 +0200

    get  postgres connection details from  config.json

--- conn.js
+++ conn.js
@@ -1,10 +1,11 @@
 const {Client} = require('pg');
+const config = require('./config');
 const conn = new Client({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'root',
-    database: 'contactbook',
-    port : 5432
+    host: config.postgres.host,
+    user: config.postgres.user,
+    password: config.postgres.password,
+    database: config.postgres.dbname,
+    port : config.postgres.port
 });
 conn.connect();
 module.exports = conn;
