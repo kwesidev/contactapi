8bc5c76979de90cebd24364f32093e1ba6140220
William Kwasidev <kwesidev@gmail.com>
2018-01-17 12:53:04 +0200

    PRIMARY KEY CONSTRAINT

--- database/db.sql
+++ database/db.sql
@@ -1,20 +1,18 @@
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+CREATE TABLE users (
+    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
+    username character varying NOT NULL,
+    password character varying NOT NULL
+);
 
 CREATE TABLE contacts (
-    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL,
+    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
     first_name character varying NOT NULL,
     last_name character varying NOT NULL,
     mobile character varying NOT NULL,
     email_address character varying NOT NULL,
-    user_id uuid DEFAULT 'a4d96ac3-cc64-4dd6-be03-8d0c15887276'::uuid NOT NULL
-);
-
-
-CREATE TABLE users (
-    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL,
-    username character varying NOT NULL,
-    password character varying NOT NULL
+    user_id uuid  NOT NULL REFERENCES users(uu_id)
 );
 
 
