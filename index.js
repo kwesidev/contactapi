053ef8886c6d6a1b7278430159722a4088fd95d2
William <kwesidev@gmail.com>
2020-02-12 09:27:01 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
 #! /usr/bin/node
- //Contact API
- // @Author William Kwasidev
+//Contact API
+// @Author William Kwasidev
 const express = require('express'),
-  app = require('express')(),
-  config = require('./config.json'),
-  contactService = require('./services.js');
-  bodyPasrser = require('body-parser'),
-  cors = require('cors'),
-  PORT = 3000,
-  va = require('validator'),
-  util = require('./utils.js'),
-  jwt = require('jsonwebtoken'),
-  morgan = require('morgan');
+    app = require('express')(),
+    config = require('./config.json'),
+    contactService = require('./services.js');
+bodyPasrser = require('body-parser'),
+    cors = require('cors'),
+    PORT = 3000,
+    va = require('validator'),
+    util = require('./utils.js'),
+    jwt = require('jsonwebtoken'),
+    morgan = require('morgan');
 var userId = null;
 app.use(express.static(__dirname + '/public'));
-// exit process
+// Exit process
 process.on('SIGINT', () => {
     console.log('Going off');
     process.exit(0);
 });
 
-//enable cors
+// Enable cors
 app.use(cors());
 
 app.use(morgan('combined'));
 
-//parse application/www-form-urlencoded
+// Parse application/www-form-urlencoded
 app.use(bodyPasrser.urlencoded({
     extended: false
 }));
-//parse json
+// Parse json
 app.use(bodyPasrser.json());
 
-//verfy jtokens
+// Verfy jtokens
 
-app.post('/api/authenticate',function(req,res){
-    
+app.post('/api/authenticate', function (req, res) {
+
     username = req.body.username;
     password = req.body.password;
-    contactService.authenticate(username,password,(err,result) =>{
-        
-        if(err) {
-            return res.status(403).json({'message:':'Invalid Credentials'});
+    contactService.authenticate(username, password, (err, result) => {
+
+        if (err) {
+            return res.status(403).json({ 'message:': 'Invalid Credentials' });
         }
-        
-        const token  = jwt.sign({id: result},config.secret,{expiresIn: '1h'});
-        return  res.status(200).json({'message':'expires in 1hour','token':token});
-        
+
+        const token = jwt.sign({ id: result }, config.secret, { expiresIn: '1h' });
+        return res.status(200).json({ 'message': 'expires in 1hour', 'token': token });
+
     });
 });
-//middleware to verify json webtoken
-app.use(function(req,res,next){
-        const access_token = req.headers['access-token'];
-        console.log(access_token);
-        jwt.verify(access_token,config.secret,(err,decoded)=>{
-                    if (err){
+// Middleware to verify json webtoken
+app.use(function (req, res, next) {
+    const access_token = req.headers['access-token'];
+    console.log(access_token);
+    jwt.verify(access_token, config.secret, (err, decoded) => {
+        if (err) {
 
-                        res.status(403).json({'message':'Invalid Token'});
-                    }
-                    else {
-                        next();
-                        userId =  decoded.id;
-                        console.log(userId);
-                      }
+            res.status(403).json({ 'message': 'Invalid Token' });
+        }
+        else {
+            next();
+            userId = decoded.id;
+            console.log(userId);
+        }
 
-        });
+    });
 
 
 });
 
 app.post("/api/contact", (req, res) => {
-    
+
     let fname = req.body.first_name,
-    lname = req.body.last_name,
-    mobile = req.body.mobile_number,
-    email = req.body.email_address;
+        lname = req.body.last_name,
+        mobile = req.body.mobile_number,
+        email = req.body.email_address;
 
     var errors = [];
     if (!va.isEmail(email))
@@ -89,50 +89,50 @@ app.post("/api/contact", (req, res) => {
     console.log(fname);
     console.log(mobile);
     if (errors.length === 0) {
-        contactService.addContact(userId,fname,lname,email,mobile,(err,result) => {
-          
-        if (err) {
-              
-            res.status(400);
-        }
-        else {
-            res.status(200).json({message:"Contact Added Succesfully"});
-        }
-      });
+        contactService.addContact(userId, fname, lname, email, mobile, (err, result) => {
+
+            if (err) {
+
+                res.status(400);
+            }
+            else {
+                res.status(200).json({ message: "Contact Added Succesfully" });
+            }
+        });
 
     } else {
 
-    console.log("*Invalid Fields \n");
-    console.log(errors);
-    res.status(400).json({
-      message: "Error",
-      error: errors
-    });
+        console.log("*Invalid Fields \n");
+        console.log(errors);
+        res.status(400).json({
+            message: "Error",
+            error: errors
+        });
 
-  }
+    }
 });
-//get contacts list
+// Get contacts list
 app.get("/api/contact", (req, res) => {
 
-    contactService.getContacts(userId,(err,result) => {
-        
-        if(err) {
+    contactService.getContacts(userId, (err, result) => {
+
+        if (err) {
             res.status(400);
         }
         else {
             res.status(200).json(result);
         }
     });
-        
+
 });
 
-app.get('/api/contact/:uuid',(req,res) => {
-    
+app.get('/api/contact/:uuid', (req, res) => {
+
     let uuid = req.params.uuid.trim();
     console.log(uuid);
-    contactService.getContact((userId,uuid,err,result) => {
-        
-        if(err) {
+    contactService.getContact((userId, uuid, err, result) => {
+
+        if (err) {
 
             res.status(400);
         }
@@ -140,16 +140,16 @@ app.get('/api/contact/:uuid',(req,res) => {
             res.status(200).json(result);
         }
     });
-    
+
 });
-//catch errros
-app.use((err,req,res,next) => {
+// Catch errros
+app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(400).send('Bad Request');
-    
+
 });
-//listen and serve requests
-app.listen( process.env.PORT || PORT, () => {
+// Listen and serve requests
+app.listen(process.env.PORT || PORT, () => {
 
     console.log("APP running on port %d", PORT || process.env.PORT);
 
--- services.js
+++ services.js
@@ -1,79 +1,79 @@
 const conn = require('./conn.js');
-//authenticate 
-module.exports.authenticate = function(username,password,cb) {
-    
-    conn.query('SELECT uu_id FROM users  WHERE lower(username) = $1 AND lower(password) = lower($2)',[username,password],(err,res) => {
-        
-        if(err){
-            cb(err,null);
+// Authenticate 
+module.exports.authenticate = function (username, password, cb) {
+
+    conn.query('SELECT uu_id FROM users  WHERE lower(username) = $1 AND lower(password) = lower($2)', [username, password], (err, res) => {
+
+        if (err) {
+            cb(err, null);
         }
-        
-        else if(res.rows.length == 0){
-            cb('Invalid Credentials',null);
+
+        else if (res.rows.length == 0) {
+            cb('Invalid Credentials', null);
         }
         else
-            cb(null,res.rows[0].uu_id);
+            cb(null, res.rows[0].uu_id);
     });
-     
+
 };
-//get contacts
-module.exports.getContacts = function(key,cb){
-    
-    conn.query('SELECT uu_id,first_name,last_name,email_address,mobile FROM contacts WHERE user_id=$1',[key],(err,res) => {
-        
-        if(err){
-            
-            cb(err,null);
+// Get contacts
+module.exports.getContacts = function (key, cb) {
+
+    conn.query('SELECT uu_id,first_name,last_name,email_address,mobile FROM contacts WHERE user_id=$1', [key], (err, res) => {
+
+        if (err) {
+
+            cb(err, null);
         }
         else {
 
-            cb(null,res.rows);
-        }        
+            cb(null, res.rows);
+        }
     });
 };
-//gets a specific contact based on id
-module.exports.getContact = function (key,uuid,cb) {
-    
+// Gets a specific contact based on id
+module.exports.getContact = function (key, uuid, cb) {
+
     let sql = 'SELECT first_name,last_name,email_address,mobile FROM contacts WHERE uu_id=$2 AND user_id = $1';
-    conn.query(sql,[key,uuid],(err,res) => {
-        
-        if(err){
-            
-            cb(err,null);
+    conn.query(sql, [key, uuid], (err, res) => {
+
+        if (err) {
+
+            cb(err, null);
         }
         else {
 
-            cb(null,res.rows);
+            cb(null, res.rows);
         }
     });
 };
-//Insert a contact 
-module.exports.addContact = function(key,firstName,lastName,emailAddress,mobileNumber,cb){
-    
+// Insert a contact 
+module.exports.addContact = function (key, firstName, lastName, emailAddress, mobileNumber, cb) {
+
     let sql = 'INSERT INTO contacts(first_name,last_name,email_address,mobile,user_id) VALUES($2,$3,$4,$5,$1)';
     //In Transaction mode
-    conn.query("BEGIN",(errTr) =>{
-        if(errTr){
-            
-            cb(errTr,null);
+    conn.query("BEGIN", (errTr) => {
+        if (errTr) {
+
+            cb(errTr, null);
         }
         else {
             conn.query(sql
-            ,[key,firstName,lastName,emailAddress,mobileNumber],(err,res) => {
-            
-            if(err){
-                
-                cb(err,null);
-            }
-    
-                
-            });
-        
-            conn.query("COMMIT",(errTr) =>{
-            
-                if(errTr){
-                
-                    cb(errTr,null);
+                , [key, firstName, lastName, emailAddress, mobileNumber], (err, res) => {
+
+                    if (err) {
+
+                        cb(err, null);
+                    }
+
+
+                });
+
+            conn.query("COMMIT", (errTr) => {
+
+                if (errTr) {
+
+                    cb(errTr, null);
                 }
 
             });
@@ -81,44 +81,44 @@ module.exports.addContact = function(key,firstName,lastName,emailAddress,mobileN
             cb();
 
         }
-        
+
     });
-    
-    
+
+
 };
 //update a particular contact
-module.exports.updateContact = function(key,uuid,firstName,lastName,emailAddress,mobileNumber,cb) {
-    
+module.exports.updateContact = function (key, uuid, firstName, lastName, emailAddress, mobileNumber, cb) {
+
     let sql = 'UPDATE contacts SET first_name=$3,last_name=$4,email_address=$5,mobile_number=$6 WHERE uu_id = $2 AND user_id = $1';
     //Update in Transaction mode
-    con.query("BEGIN",(errTr) => {
-        
-        if(errT) {
-            
-            cb(errT,null);
-            
+    con.query("BEGIN", (errTr) => {
+
+        if (errT) {
+
+            cb(errT, null);
+
         }
         else {
-            conn.query(sql,[firstName,lastName,emailAddress,mobileNumber,uuid],(err,res) => {
-            
-            if(err){
-                
-                cb(err,null);
-            }
-                
-            
-            });
-            conn.query("COMMIT",(errTr) => {
-            
-                if (errTr){
-                
-                    cb(errTr,null);
+            conn.query(sql, [firstName, lastName, emailAddress, mobileNumber, uuid], (err, res) => {
+
+                if (err) {
+
+                    cb(err, null);
                 }
-            
+
+
+            });
+            conn.query("COMMIT", (errTr) => {
+
+                if (errTr) {
+
+                    cb(errTr, null);
+                }
+
             });
             cb();
         }
     });
-    
+
 };
 
