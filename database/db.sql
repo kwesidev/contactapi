bfaa9f1c09498eb00b289a3dfd6053a04f1c11d9
William <kwesidev@gmail.com>
2020-05-09 09:13:23 +0200

    update sql script

--- database/db.sql
+++ database/db.sql
@@ -1,22 +1,31 @@
 
+
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+CREATE SCHEMA system;
+SET search_path TO system;
+-- Creates users table
 CREATE TABLE users (
-    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
-    username character varying NOT NULL,
-    password character varying NOT NULL
+    user_id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
+    username VARCHAR NOT NULL,
+    password VARCHAR NOT NULL,
+	active BOOLEAN NOT NULL DEFAULT TRUE,
+	created_on TIMESTAMP NOT NULL,
+    schema_name VARCHAR NOT NULL
 );
+
+INSERT INTO users(username, password, created_on, schema_name) VALUES('kwesidev', 'password', NOW(), 'customer1');
+
+CREATE SCHEMA customer1 ;
+SET search_path TO customer1;
 
 CREATE TABLE contacts (
-    uu_id uuid DEFAULT uuid_generate_v4() NOT NULL PRIMARY KEY,
-    first_name character varying NOT NULL,
-    last_name character varying NOT NULL,
-    mobile character varying NOT NULL,
-    email_address character varying NOT NULL,
-    user_id uuid  NOT NULL REFERENCES users(uu_id)
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
+    first_name VARCHAR NOT NULL,
+    last_name VARCHAR  NOT NULL,
+    mobile_number VARCHAR  NOT NULL,
+    email_address VARCHAR  NOT NULL
 );
 
-
-INSERT INTO users(uu_id,username,password) VALUES('6b9437ce-fb74-11e7-8c3f-9a214cf093ae','kwesidev','password');
-INSERT INTO contacts(first_name,last_name,mobile,email_address,user_id) VALUES('jackie','kwez','0658333670','anonymous@gmail.com','6b9437ce-fb74-11e7-8c3f-9a214cf093ae');
+INSERT INTO contacts(first_name, last_name, mobile_number, email_address) VALUES('jackie','kwez','0658333670','anonymous@gmail.com');
 
 
