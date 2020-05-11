0bd5c37d3c59200cb169342531a80fd9895de1f7
William <kwesidev@gmail.com>
2020-05-11 19:28:14 +0200

    Add tenants table to system schema

--- database/db.sql
+++ database/db.sql
@@ -2,19 +2,28 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
 CREATE SCHEMA system;
 SET search_path TO system;
 -- Creates users table
+
+CREATE TABLE tenants(
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
+    schema_name VARCHAR NOT NULL,
+    created_on VARCHAR NOT NULL
+);
+
+INSERT INTO tenants(id, schema_name, created_on) VALUES('74b73f8a-758e-45ce-86bf-b6c0799bc79c','tenant_1',NOW());
+
 CREATE TABLE users (
-    user_id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
+    id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
     username VARCHAR NOT NULL,
     password VARCHAR NOT NULL,
     active BOOLEAN NOT NULL DEFAULT TRUE,
     created_on TIMESTAMP NOT NULL,
-    schema_name VARCHAR NOT NULL
+    tenant_id uuid NOT NULL REFERENCES tenants(id)
 );
 
-INSERT INTO users(username, password, created_on, schema_name) VALUES('kwesidev', 'password', NOW(), 'customer1');
+INSERT INTO users(username, password, created_on, tenant_id) VALUES('kwesidev', 'password', NOW(), '74b73f8a-758e-45ce-86bf-b6c0799bc79c');
 
-CREATE SCHEMA customer1 ;
-SET search_path TO customer1;
+CREATE SCHEMA tenant1 ;
+SET search_path TO tenant1;
 
 CREATE TABLE contacts (
     id uuid DEFAULT public.uuid_generate_v4() NOT NULL PRIMARY KEY,
