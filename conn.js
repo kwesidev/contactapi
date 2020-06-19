ea1d36d3c8ff79272ab2ddb3a856ec6a6ea5fe85
William <kwesidev@gmail.com>
2020-06-19 13:01:18 +0200

    Connection pool

--- conn.js
+++ conn.js
@@ -1,11 +1,11 @@
-const {Client} = require('pg'),
+const {Pool} = require('pg'),
 config = require('./config');
-conn = new Client({
+conn = new Pool({
     host: config.postgres.host,
     user: config.postgres.user,
     password: config.postgres.password,
     database: config.postgres.dbname,
     port : config.postgres.port
 });
-conn.connect();
+//conn.connect();
 module.exports = conn;
